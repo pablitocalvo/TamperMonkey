28a39035388bf34ebf2944002440fb168dca2711
pablitocalvo <pablitocalvo@users.noreply.github.com>
2019-03-05 15:02:16 +0100

    primo commit

---
+++ registro.js
@@ -0,0 +1,79 @@
+// ==UserScript==
+// @name         Registro
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://re22.axioscloud.it/Secret/REMenu.aspx*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function bottoneOnClick(){
+
+        const _3Bs = 0; const _4Bs = 1; const _4Bt = 2; const _4Cs = 3;const _4Ct = 4; const _5Bs = 5; const _5Bt = 6; const _5Cs = 7; const _5Ct = 8;
+
+        var d = new Date(); var t = d.getTime(); var giorno=d.getDay()-1; var ora=d.getHours(); var minuti=d.getMinutes();
+
+
+        giorno=0;
+        ora=9;
+        minuti=33;
+
+        if (( giorno < 0 ) || (giorno > 4)) {
+            return;
+        }
+
+        //traduce ora:minuti in orascolastica € [0,8]
+        const i_oraIng = 8*60 ;
+        const i_oraUsc = 14*60 + 18 ;
+        
+        var i_orario =  ora*60 + minuti;
+
+        if ((i_orario < i_oraIng ) || (i_orario > i_oraUsc ))  {
+            return;
+        }
+
+        var oraScol= Math.floor((i_orario - i_oraIng) /54) ;
+
+        // orario[giorno , ora ]
+        var orario = [[  -1 , _5Cs, _5Bs, _4Bs,  -1 ,  -1  ,  -1   ], //lunedi
+                      [ _5Bt, _4Ct, _4Bt,  -1 ,  -1 ,  -1  ,  -1   ], //martedi
+                      [ _5Ct, _5Ct, _4Bs, _4Cs,  -1 ,  -1  ,  -1   ], //mercoledi
+                      [ _4Cs, _3Bs, _3Bs,  -1 ,  -1 ,  -1  ,  -1   ], //giovedi
+                      [ _4Bt, _5Bs, _4Ct,  -1 ,  -1 , _5Bt , _5Cs  ], //venerdi
+                     ];
+
+        var classeSelect=document.getElementById("ContentPlaceHolderMenu_ddlClassi");
+
+        //alert("selezionato="+classeSelect.options[classeSelect.selectedIndex].text);
+
+alert("g ="+giorno+"ora="+oraScol+" idnex="+orario[giorno][oraScol]);
+
+        if (orario[giorno][oraScol] < 0) {
+            alert("non sei in classe");
+            return
+        }
+
+        classeSelect.selectedIndex= orario[giorno][oraScol];
+         //$( "#ContentPlaceHolderMenu_ddlClassi" ).trigger("change");
+
+        // ora bisgno triggerare onClick sul registro ....
+       $( "input[name='ctl00$ContentPlaceHolderBody$Button_RE_Classe_G']").trigger("click");
+      //  $(document).trigger("onclick");
+    }
+
+
+    // Your code here...
+    var headerRow=document.getElementById("headerRow");
+    var button=document.createElement("BUTTON");
+    button.style.height = "45px";
+    button.style.width = "45px";
+
+    button.addEventListener("click", bottoneOnClick);
+    headerRow.insertBefore(button, headerRow.childNodes[0]);
+
+
+
+})();
