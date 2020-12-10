7a8a23692f145d9b44271e34645cd2aac2cf65ae
pablitocalvo <pablitocalvo@users.noreply.github.com>
2020-12-10 21:17:14 +0100

    Update registro.js

--- registro.js
+++ registro.js
@@ -1,3 +1,4 @@
+
 // ==UserScript==
 // @name         Registro
 // @namespace    http://tampermonkey.net/
@@ -12,28 +13,30 @@
 (function() {
     'use strict';
 
-     const _3BS = 0; const _4BS = 1; const _4BT = 2; const _4CS = 3;const _4CT = 4; const _5BS = 5; const _5BT = 6; const _5CS = 7; const _5CT = 8;
+     const _3BS = 0; const _4BS = 2; const _4BT = 3; const _5AS = 5;const _5AT = 6; const _5BS = 8; const _5BT = 9 ; const _5CS = 11; const _5CT = 12;
 
-     const orario = [[  -1  ,   -1 ,   -1 ,  -1 ,  -1  ,  -1  , -1     ], //domenica
-                     [  -1  ,   -1 ,   -1 ,  -1 ,  -1  , _5CT , _5CS   ], //lunedi
-                     [  -1  ,   -1 , _4CT , _5BS, _5BT ,  -1  , _4BS   ], //martedi
-                     [ _5CS , _4BS , _5CT , _4CS, _4CS ,  -1  ,  -1    ], //mercoledi
-                     [  -1  , _5BT , _3BS , _4BT,  -1  , _5BS , _3BS   ], //giovedi
-                     [  -1  ,   -1 ,   -1 ,  -1 , _4CT , _4BT , -1     ], //venerdi
-                     [  -1  ,   -1 ,   -1 ,  -1 ,  -1  ,  -1  , -1     ], //sabato
+     const orario = [[  -1  ,   -1 ,   -1 ,  -1  ,  -1  ,  -1  , -1     ], //domenica
+                     [  -1  , _5BS , _5CT , _5AS , _5BT ,  -1  , -1     ], //lunedi 2020
+                     [ _5AS , _5AS , _5BS , _5CS , _5CS ,  -1  , -1     ], //MARTEDI 2020
+                     [  -1  ,   -1 , _4BS , _3BS ,  -1  ,  -1  , -1     ], //MERCOLEDI 2020
+                     [ _5CT , _5AS ,  -1  , _5BS ,  -1  ,  -1  , -1     ], //GIOVEDI 2020
+                     [ _3BS ,   -1 , _4BT , _4BT , _4BS ,  -1  , -1     ], //VENERDI 2020
+                     [  -1  ,   -1 ,   -1 ,  -1  ,  -1  ,  -1  , -1     ], //sabato
                     ];
 
     function deselezionaClasse(){
-            return; //non funziona ancora !!!
+      //      return; //non funziona ancora !!!
 
-//     var selezioneClasse=document.getElementById("ContentPlaceHolderMenu_ddlClassi");
+     var selezioneClasse=document.getElementById("ContentPlaceHolderMenu_ddlClassi");
 
-//     var opzioneNulla=document.createElement("OPTION");
+     var opzioneNulla=document.createElement("OPTION");
 
-//     opzioneNulla.innerHTML="Selezionare una Classe-Materia";
-//     opzioneNulla.selected=true;
+     opzioneNulla.innerHTML="Selezionare una Classe-Materia";
+     opzioneNulla.selected=true;
 
-//     selezioneClasse.insertBefore(opzioneNulla,selezioneClasse.childNodes[0]);
+
+     selezioneClasse.insertBefore(opzioneNulla,selezioneClasse.childNodes[0]);
+        alert(" selezionat =");
 //     // triggera on click su ....
 //         $( "input[name='ctl00$ContentPlaceHolderBody$Button_RE_Classe_G']").trigger("click");
 
@@ -77,7 +80,7 @@
 
 
     function oraSelezionata( ora0_7 )
-    {   //TODO: inserire controllo ora compresa 0_7
+    {//TODO: inserire controllo ora compresa 0_7
 //alert("ora selezionat ="+ora);
         var d = new Date();
         var giorno=d.getDay();
@@ -96,9 +99,9 @@
         var opzioniClasse=document.querySelectorAll("#ContentPlaceHolderMenu_ddlClassi > optgroup > option");
         opzioniClasse[indice].selected = true;
 
-        // TODO: è veramente necessario? triggera on click su ....
-        $( "input[name='ctl00$ContentPlaceHolderBody$Button_RE_Classe_G']").trigger("click");
-    }
+        // TODO: è veramente necessario? triggera on click su ....si SE VUOI APRIRE IL REGISTRO
+        $( "input[name='ctl00$ContentPlaceHolderBody$Button_RE_Classe_G_New']").trigger("click");
+     }
 
 
 //****************************************
@@ -180,152 +183,6 @@
     miaBarra.insertBefore(button_1, miaBarra.childNodes[0]);
 }
 
-  // controlla azioni da compiere
-
-        if (sessionStorage.Azione=="SELEZIONA"){
-            sessionStorage.Azione="NULLA";
-            var indice= sessionStorage.Classe;
-
-//alert( indice);
-            sessionStorage.removeItem('Classe');
-//alert( eval(indice));
-             selezionaClasseDaIndice( eval(indice) );
-        }
-
-
-
-})();
-        if ((i_orario < i_oraIng ) || (i_orario > i_oraUsc ))  {
-            deselezionaClasse();return;
-        }
-
-        // orascol è la ora scolastica [ 0 , 7 )
-        var oraScol= Math.floor((i_orario - i_oraIng) /54) ;
-
-        if (orario[giorno][oraScol] < 0) {
-             deselezionaClasse();alert("non sei in classe");
-            return
-        }
-
-        selezionaClasseDaIndice(orario[giorno][oraScol]);
-    }
-
-
-    function oraSelezionata( ora0_7 )
-    {   //TODO: inserire controllo ora compresa 0_7
-//alert("ora selezionat ="+ora);
-        var d = new Date();
-        var giorno=d.getDay();
-
-//alert ( "g="+giorno+" h="+ora);
-
-        if (orario[giorno][ora0_7] < 0){
-            deselezionaClasse();return;
-        }
-//alert("ora chiamo seleziona classe da indice="+orario[giorno][ora]);
-        selezionaClasseDaIndice(orario[giorno][ora0_7]);
-    }
-
-    function selezionaClasseDaIndice( indice ){
-//alert("selDaIndice"+indice);
-        var opzioniClasse=document.querySelectorAll("#ContentPlaceHolderMenu_ddlClassi > optgroup > option");
-        opzioniClasse[indice].selected = true;
-
-        // TODO: è veramente necessario? triggera on click su ....
-        $( "input[name='ctl00$ContentPlaceHolderBody$Button_RE_Classe_G']").trigger("click");
-    }
-
-
-//****************************************
-  // Your code here...
-//****************************************
-
-    // aggiunge una barra personalizzata
-{    var miaBarra= document.createElement("DIV");
-    //var att = document.createAttribute("class");
-    //att.value = "RE_TRow_h45";
-    //miaBarra.setAttributeNode(att);
-
-
-    //var containerBar = document.querySelector("#menuRow > div > div.RE_Table_MasterPage");
-    var containerBar = document.querySelector("#GlobalContent > div > div:nth-child(2)");
-    containerBar.insertBefore(miaBarra,containerBar.childNodes[0]);
-
-
-   // var headerRow=document.getElementById("headerRow");
-    //bottonr ORA
-    var button=document.createElement("BUTTON");
-    button.style.height = "45px";
-    button.style.width = "45px";
-
-    button.addEventListener("click", bottoneOra);
-    miaBarra.insertBefore(button, miaBarra.childNodes[0]);
-
-
-
-
-//bottone
-    var button_7=document.createElement("BUTTON");
-    button_7.innerHTML="7";
-    button_7.style.height = "20px";
-    button_7.style.width = "20px";
-    button_7.addEventListener("click", function() { oraSelezionata(6); });
-    miaBarra.insertBefore(button_7, miaBarra.childNodes[0]);
-//bottone
-    var button_6=document.createElement("BUTTON");
-    button_6.innerHTML="6";
-    button_6.style.height = "20px";
-    button_6.style.width = "20px";
-    button_6.addEventListener("click", function() { oraSelezionata(5); });
-    miaBarra.insertBefore(button_6, miaBarra.childNodes[0]);
-//bottone
-    var button_5=document.createElement("BUTTON");
-    button_5.innerHTML="5";
-    button_5.style.height = "20px";
-    button_5.style.width = "20px";
-    button_5.addEventListener("click", function() { oraSelezionata(4); });
-    miaBarra.insertBefore(button_5, miaBarra.childNodes[0]);
-//bottone
-    var button_4=document.createElement("BUTTON");
-    button_4.innerHTML="4";
-    button_4.style.height = "20px";
-    button_4.style.width = "20px";
-    button_4.addEventListener("click", function() { oraSelezionata(3); });
-    miaBarra.insertBefore(button_4, miaBarra.childNodes[0]);
-//bottone
-    var button_3=document.createElement("BUTTON");
-    button_3.innerHTML="3";
-    button_3.style.height = "20px";
-    button_3.style.width = "20px";
-    button_3.addEventListener("click", function() { oraSelezionata(2); });
-    miaBarra.insertBefore(button_3, miaBarra.childNodes[0]);
- //bottone
-    var button_2=document.createElement("BUTTON");
-    button_2.innerHTML="2";
-    button_2.style.height = "20px";
-    button_2.style.width = "20px";
-    button_2.addEventListener("click", function() { oraSelezionata(1); });
-    miaBarra.insertBefore(button_2, miaBarra.childNodes[0]);
-  //bottone
-    var button_1=document.createElement("BUTTON");
-    button_1.innerHTML="1";
-    button_1.style.height = "20px";
-    button_1.style.width = "20px";
-    button_1.addEventListener("click", function() { oraSelezionata(0); });
-    miaBarra.insertBefore(button_1, miaBarra.childNodes[0]);
-}
-
-  // controlla azioni da compiere
-
-        if (sessionStorage.Azione=="SELEZIONA"){
-            sessionStorage.Azione="NULLA";
-            var indice= sessionStorage.Classe;
-
-//alert( indice);
-            sessionStorage.removeItem('Classe');
-//alert( eval(indice));
-             selezionaClasseDaIndice( eval(indice) );
-        }
 
 
 
